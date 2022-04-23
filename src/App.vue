a0a9492155178e7843596a2b65449b6d7478932d
AniaSniadek <annsni2@st.amu.edu.pl>
2022-04-23 13:16:20 +0200

    add cars in home page

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import carsData from '../src/assets/data.json'
 </script>
 
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link>
-    <router-link to="/mustang">Mustang</router-link>
-    <router-link to="/ferrari">Ferrari</router-link>
-    <router-link to="/lamborghini">Lamborghini</router-link>
-    <router-link to="/porsche">Porsche</router-link>
+    <router-link v-for="car in carsData.cars" :key="car.id" :to="car.link">{{
+      car.name
+    }}</router-link>
   </div>
   <div class="container">
     <RouterView />
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import carsData from '../assets/data.json'
+</script>
 
 <template>
-  <main>Hello</main>
+  <div class="home">
+    <h1>Home</h1>
+    <div class="destinations">
+      <router-link v-for="car in carsData.cars" :key="car.id" :to="car.link">
+        <h2>{{ car.name }}</h2>
+        <img :src="`../src/assets/images/${car.img}`" :alt="car.name" />
+      </router-link>
+    </div>
+  </div>
 </template>

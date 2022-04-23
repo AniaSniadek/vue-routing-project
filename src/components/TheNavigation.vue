ba025ab3fda05af3e8e10520c60c5c6a15132bb6
AniaSniadek <annsni2@st.amu.edu.pl>
2022-04-23 14:27:11 +0200

    routing to cars

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -5,8 +5,11 @@ import carsData from '@/assets/data.json'
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link>
-    <router-link v-for="car in carsData.cars" :key="car.id" :to="car.link">{{
-      car.name
-    }}</router-link>
+    <router-link
+      v-for="car in carsData.cars"
+      :key="car.id"
+      :to="{ name: 'car.view', params: { id: car.id } }"
+      >{{ car.name }}</router-link
+    >
   </div>
 </template>
